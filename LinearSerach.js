d809d1c728dad0bf76813222b69bd8a674174f27
Kondakalla Prabhakar <prabhakarkondakalla9@gmail.com>
2025-01-21 07:26:47 +0530

    LinearSerachChanged

--- LinearSerach.js
+++ LinearSerach.js
@@ -1,20 +1,5 @@
-// function findElment(array,target){
-//     for(let i =0;i<array.length;i++){
-//         if(array[i]===target){
-//             return i;
-//         }    
-//     }
-//     return -1;
-// }
-// const array=[4,7,2,8,3,5];
-// const target=5;
-//  const output=findElment(array,target);
-//  if(output!=-1){
-//     console.log("The Element found at index"+output);
-//  }else{
-//     console.log("The Element not Found");
-//  }
- function LinearSerach(array,target){
+ 
+ function LinearSerachChanged(array,target){
     for(let i=0;i<array.length;i++){
         if(array[i]===target){
             return "The Element is Found At the Index"+i;
@@ -24,4 +9,4 @@
  }
  const array=[4,7,2,8,3,5];
  const target=5
- console.log(LinearSerach(array,target));
+ console.log(LinearSerachChanged(array,target));
